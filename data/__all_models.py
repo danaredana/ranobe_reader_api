6f57c1c81a5e5cc1aa2d2c2d9a459ca7d41e58cc
danaredana <subingerman@gmail.com>
2025-05-14 21:47:47 +0300

    Create __all_models.py

---
+++ data/__all_models.py
@@ -0,0 +1,7 @@
+from .users import User
+from .ranobe import Ranobe
+from .volume import Volume
+from .chapter import Chapter
+from .comment import Comment
+
+__all__ = ['User', 'Ranobe', 'Volume', 'Chapter', 'Comment']
